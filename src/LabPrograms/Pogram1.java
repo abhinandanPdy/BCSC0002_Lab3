36947107ac9427b4b4d17285ed5e9e70c6b3edea
AbhinandanPadmakarPandey <abhinandan.pandey_cs18@gla.ac.in>
2019-09-03 22:36:28 +0530

    Creating object of scanner.

--- src/LabPrograms/Pogram1.java
+++ src/LabPrograms/Pogram1.java
@@ -2,5 +2,10 @@ package LabPrograms;
 
 import java.util.Scanner;
 public class Pogram1 {
+    public static void main(String[] args){
 
+        System.out.println("Enter the value");
+        Scanner input=new Scanner(System.in);
+
+    }
 }
