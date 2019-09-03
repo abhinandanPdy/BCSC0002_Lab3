6d1bed26d14fae85d8b9a2c306c033346a0db917
AbhinandanPadmakarPandey <abhinandan.pandey_cs18@gla.ac.in>
2019-09-03 23:53:02 +0530

    Separate integer and decimal part of the price.
    Printing output as required.

--- src/LabPrograms/Program2.java
+++ src/LabPrograms/Program2.java
@@ -6,4 +6,12 @@ public class Program2 {
         System.out.println("Enter the price.");
         Scanner input = new Scanner(System.in);
         double price = input.nextDouble();
+        int iPrice = (int) price;
+        double fPrice = price - iPrice;
+        if (fPrice == 0) {
+            System.out.println(price + " Rupees and 0 Paise");
+        } else {
+            System.out.println(iPrice + " Rupees and " + (float)fPrice + " Paise");
+        }
+    }
 }
