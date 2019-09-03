c6cc8c5018c2452724025baf2732679e4c4c8987
AbhinandanPadmakarPandey <abhinandan.pandey_cs18@gla.ac.in>
2019-09-04 00:12:47 +0530

    Making scanner object.
    Taking input from user.
    
    Printing output.

--- src/LabPrograms/Program3.java
+++ src/LabPrograms/Program3.java
@@ -2,4 +2,12 @@ package LabPrograms;
 
 import java.util.Scanner;
 public class Program3 {
+    public static void main(String[] args) {
+        //Converting Fahrenheit to Celsius.
+        System.out.println("Enter the temp. in fahrenheit");
+        Scanner input=new Scanner(System.in);
+        double f=input.nextDouble();
+        double c = (f-32)/1.8;
+        System.out.println(c);
+    }
 }
