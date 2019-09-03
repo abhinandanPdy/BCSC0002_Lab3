daf969daecec928ef341f6befe674a4713c721ee
AbhinandanPadmakarPandey <abhinandan.pandey_cs18@gla.ac.in>
2019-09-03 23:50:36 +0530

    Creating object of scanner.
    Taking input from user.

--- src/LabPrograms/Program2.java
+++ src/LabPrograms/Program2.java
@@ -2,5 +2,8 @@ package LabPrograms;
 
 import java.util.Scanner;
 public class Program2 {
-
+    public static void main(String[] args) {
+        System.out.println("Enter the price.");
+        Scanner input = new Scanner(System.in);
+        double price = input.nextDouble();
 }
